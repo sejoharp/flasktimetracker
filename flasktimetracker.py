9235f4d915d57577dd5335c43cb2b277ac0daaac
joscha <joscha@localhost>
2011-04-10 00:35:40 +0200

    mongodb-connection implemented via mongoengine

--- flasktimetracker.py
+++ flasktimetracker.py
@@ -3,17 +3,38 @@ from flask import session, escape, flash, redirect, url_for
 from functools import wraps
 from flaskext.wtf import Form,TextField,PasswordField
 from wtforms import validators
+from werkzeug import generate_password_hash, check_password_hash
+import datetime
 import os
+from mongoengine import EmbeddedDocument, Document, DateTimeField, StringField, IntField, ListField, EmbeddedDocumentField, connect
 
 # configuration
 DEBUG = True
 SECRET_KEY = "66&r+=abre8AKaprE!acR87=P?esAd"
 CSRF_ENABLED = False
+MONGODB_DATABASE = "testdb"
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('SETTINGS', silent=True)
+
+class Interval(EmbeddedDocument):
+    start = DateTimeField()
+    stop = DateTimeField()
+    
+class User(Document):
+    username = StringField()
+    pw_hash = StringField()
+    overtime = IntField()
+    worktime = IntField()
+    intervals = ListField(EmbeddedDocumentField(Interval))
+    
+    def set_pw_hash(self, password):
+        self.pw_hash = generate_password_hash(password)
+        
+    def is_correct_password(self, password):
+        return check_password_hash(self.pw_hash, password)
 
 class LoginForm(Form):
     username = TextField("username",[validators.required()])
@@ -29,6 +50,8 @@ def requires_auth(f):
             return f(*args, **kwargs)
     return decorated
 
+mongo = connect(app.config['MONGODB_DATABASE'])
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -40,6 +63,7 @@ def login():
 
 @app.route('/logout')
 def logout():
+    session.pop("id", None)
     flash("logged out successfully.")
     return redirect(url_for('login'))
 
--- flasktimetracker_tests.py
+++ flasktimetracker_tests.py
@@ -1,37 +1,89 @@
 import unittest
 import flasktimetracker
+from flasktimetracker import Interval, User
+from datetime import datetime, timedelta
+from flask import Flask
+from werkzeug import generate_password_hash, check_password_hash
 
 class FlaskTimeTrackerTestCase(unittest.TestCase):
     
     def setUp(self):
-        self.app = flasktimetracker.app.test_client()
+        self.appClient = flasktimetracker.app.test_client()
 
+    def assert200(self, status_code):
+        self.assertEqual(status_code, 200)
+        
+    def assert_site_available(self, site):
+        response = self.appClient.get(site)
+        self.assert200(response.status_code)
+        
     def login(self, username, password):
-        return self.app.post('/checkLogin', data=dict(username=username, password=password), follow_redirects=True)
+        return self.appClient.post('/checkLogin', data=dict(username=username, password=password), follow_redirects=True)
 
     def logout(self):
-        return self.app.get('/logout', follow_redirects=True)
+        return self.appClient.get('/logout', follow_redirects=True)
 
+    def flush_db(self):
+#        Interval.drop_collection()assert
+        User.drop_collection()
+    
+    def set_initial_data(self):
+        admin = User(username="admin", overtime=0, worktime=28800)
+        admin.set_pw_hash("geheim")
+        
+        interval1 = Interval()
+        interval1.start = datetime(2011, 4, 4, 19, 56, 23, 709000)
+        interval1.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1)
+
+        interval2 = Interval()
+        interval2.start = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=2)
+        interval2.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5)
+                
+        admin.intervals.append(interval1)
+        admin.intervals.append(interval2)
+        admin.save()
+        
+    def test_initial_data(self):
+        self.flush_db()
+        self.set_initial_data()
+        
+        user = User.objects().first()
+        self.assertEqual(user.username, "admin")
+        self.assertTrue(check_password_hash(user.pw_hash, "geheim"))
+        self.assertEqual(user.overtime, 0)
+        self.assertEqual(user.worktime, 28800)
+        self.assertEqual(len(user.intervals), 2)
+        
+        interval = user.intervals.pop()
+        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000)+ timedelta(hours=2))
+        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5))
+        
+        interval = user.intervals.pop()
+        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000))
+        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1))
+       
+    def test_login_available(self):
+        self.assert_site_available("/login")
+        
     def test_login_valid_user_pw(self):
         response = self.login("admin","geheim")
-        assert 'logged in successfully.' in response.data
+        self.assertTrue('logged in successfully.' in response.data, response.data)
         
     def test_login_invalid_user(self):       
         response = self.login("admin" + 'x',"geheim")
-        assert 'user/pw invalid.' in response.data
+        self.assertTrue('user/pw invalid.' in response.data, response.data)
 
     def test_login_invalid_pw(self):       
         response = self.login("admin", "geheim" + 'x')
-        assert 'user/pw invalid.' in response.data
+        self.assertTrue('user/pw invalid.' in response.data, response.data)
 
     def test_login_empty_user(self):       
         response = self.login("", "geheim")
-        print response.data
-        assert 'This field is required.' in response.data
+        self.assertTrue('This field is required.' in response.data, response.data)
         
     def test_login_empty_pw(self):       
         response = self.login("admin", "")
-        assert 'This field is required.' in response.data
+        self.assertTrue('This field is required.' in response.data, response.data)
         
 if __name__ == '__main__':
     unittest.main()
