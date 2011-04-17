74bc14aada8ae3caaacc433ad9cc40b02de86ae3
joscha <joscha@localhost>
2011-04-17 20:08:50 +0200

    interval calucations added

--- flasktimetracker.py
+++ flasktimetracker.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, send_from_directory, request, Response
-from flask import session, escape, flash, redirect, url_for
+from datetime import datetime, timedelta
+from flask import Flask, render_template, send_from_directory, request, Response, \
+    session, escape, flash, redirect, url_for, abort
+from flaskext.wtf import Form, TextField, PasswordField, DateTimeField
 from functools import wraps
-from flaskext.wtf import Form,TextField,PasswordField
-from wtforms import validators
+from mongoengine import fields, Document, connect, ValidationError
+from pymongo.objectid import ObjectId
 from werkzeug import generate_password_hash, check_password_hash
-import datetime
+from wtforms import validators
 import os
-from mongoengine import EmbeddedDocument, Document, DateTimeField, StringField, IntField, ListField, EmbeddedDocumentField, connect
 
 # configuration
 DEBUG = True
@@ -19,16 +20,19 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('SETTINGS', silent=True)
 
-class Interval(EmbeddedDocument):
-    start = DateTimeField()
-    stop = DateTimeField()
+class Interval(Document):
+    start = fields.DateTimeField(required=True)
+    stop = fields.DateTimeField()
+    userid = fields.ObjectIdField(required=True)
+    
+    def duration(self):
+        return (self.stop or datetime.today()) - self.start 
     
 class User(Document):
-    username = StringField()
-    pw_hash = StringField()
-    overtime = IntField()
-    worktime = IntField()
-    intervals = ListField(EmbeddedDocumentField(Interval))
+    username = fields.StringField()
+    pw_hash = fields.StringField()
+    overtime = fields.IntField()
+    worktime = fields.IntField()
     
     def set_pw_hash(self, password):
         self.pw_hash = generate_password_hash(password)
@@ -39,7 +43,12 @@ class User(Document):
 class LoginForm(Form):
     username = TextField("username",[validators.required()])
     password = PasswordField("password",[validators.required()])
-    
+
+class Interval_Form(Form):
+    start = DateTimeField([validators.required()])
+    stop = DateTimeField()
+    userid = TextField()
+        
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -57,40 +66,64 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/login')
+@app.route('/login/show')
 def login():
         return render_template('login.html', form=LoginForm())
 
-@app.route('/logout')
+@app.route('/login/disable')
 def logout():
     session.pop("userid", None)
     flash("logged out successfully.")
     return redirect(url_for('login'))
 
-@app.route('/checkLogin',  methods=['POST'])
+@app.route('/interval/edit/<id>')
+@requires_auth
+def edit_interval(id):
+    try:
+        interval = Interval.objects().with_id(id)
+    except ValidationError:
+        abort(404)        
+    return render_template('interval_edit.html', form=Interval_Form(obj=interval))
+        
+@app.route('/interval/save',  methods=['POST'])
+@requires_auth
+def save_interval():
+    form = LoginForm(request.form) 
+    if form.validate_on_submit():
+        form.populate_obj(interval)
+        interval.save()
+        flash("interval edit successfully.")
+        return redirect(url_for('index')) 
+    else:
+        return render_template('interval_edit.html', form=form)
+
+@app.route('/login/check',  methods=['POST'])
 def check_login():
     form = LoginForm(request.form) 
     if form.validate_on_submit():
         user = User.objects(username = form.username.data).first()
         if user is not None and user.is_correct_password(form.password.data):
-            valid_login = True
-        else:
-            valid_login = False
-    else:
-        valid_login = False    
-      
-    if valid_login:
-        session["userid"] = user.id
-        flash("logged in successfully.")
-        return redirect(url_for('hello_world'))        
-    else:
-        flash("user/pw invalid.")
-        return render_template('login.html', form=form)
+            session["userid"] = user.id
+            flash("logged in successfully.")
+            return redirect(url_for('index'))  
+
+    flash("user/pw invalid.")
+    return render_template('login.html', form=form)
         
 @app.route('/')
 @requires_auth
-def hello_world():
+def index():
     return render_template('index.html')
 
+def calc_durations(intervals):
+    sum = timedelta()
+    if intervals:
+        for interval in intervals:
+            sum += interval.duration()
+    return sum
+
+def get_current_user():
+    return User.objects().with_id(session["userid"])
+    
 if __name__ == '__main__':
     app.run()
--- flasktimetracker_tests.py
+++ flasktimetracker_tests.py
@@ -12,27 +12,125 @@ class FlaskTimeTrackerTestCase(unittest.TestCase):
         self.flush_db()
         self.set_initial_data()
 
+    def get(self, url):
+        return self.appClient.get(url, follow_redirects=True)
+    
+    def post(self, url, parameter):
+        return self.appClient.post(url,data=parameter, follow_redirects=True)
+    
     def assert200(self, status_code):
         self.assertEqual(status_code, 200)
         
     def assert_site_available(self, site):
-        response = self.appClient.get(site)
+        response = self.get(site)
         self.assert200(response.status_code)
         
     def login(self, username, password):
-        return self.appClient.post('/checkLogin', data=dict(username=username, password=password), follow_redirects=True)
+        return self.post('/login/check', dict(username=username, password=password))
 
+    def login_admin(self):
+        return self.login("admin","geheim")
+        
     def logout(self):
-        return self.appClient.get('/logout', follow_redirects=True)
+        return self.get('/login/disable')
 
     def flush_db(self):
-#        Interval.drop_collection()assert
+        Interval.drop_collection()
         User.drop_collection()
     
     def set_initial_data(self):
         admin = User(username="admin", overtime=0, worktime=28800)
         admin.set_pw_hash("geheim")
+        admin.save()
         
+        interval1 = Interval()
+        interval1.start = datetime(2011, 4, 4, 19, 56, 23, 709000)
+        interval1.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1)
+        interval1.userid = admin.id
+        interval1.save()
+
+        interval2 = Interval()
+        interval2.start = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=2)
+        interval2.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5)
+        interval2.userid = admin.id
+        interval2.save()
+                      
+    def test_initial_data(self):
+        user_count = User.objects().all().count()
+        self.assertEqual(1, user_count, "1 != " + str(user_count))
+        user = User.objects().first()
+        self.assertEqual(user.username, "admin")
+        self.assertTrue(check_password_hash(user.pw_hash, "geheim"))
+        self.assertEqual(user.overtime, 0)
+        self.assertEqual(user.worktime, 28800)
+        
+        intervals = Interval.objects(userid=user.id)
+        self.assertEqual(len(intervals), 2)
+        
+        interval = intervals.next()
+        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000))
+        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1))
+        
+        interval = intervals.next()
+        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000)+ timedelta(hours=2))
+        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5))
+       
+    def test_login_available(self):
+        """ tests availability of site login """
+        self.assert_site_available("/login/show")
+        
+    def test_login0(self):
+        """ tests login with valid user and pw """
+        response = self.login("admin","geheim")
+        self.assertTrue('logged in successfully.' in response.data, response.data)
+
+    def test_login1(self):
+        """ tests login and logout with valid data """
+        response = self.login("admin","geheim")
+        self.assertTrue('logged in successfully.' in response.data, response.data)
+        response = self.logout()
+        self.assertTrue('logged out successfully.' in response.data, response.data)
+                       
+    def test_login2(self):
+        """ tests login with invalid user """       
+        response = self.login("admin" + 'x',"geheim")
+        self.assertTrue('user/pw invalid.' in response.data, response.data)
+
+    def test_login3(self):   
+        """ tests login with invalid pw """    
+        response = self.login("admin", "geheim" + 'x')
+        self.assertTrue('user/pw invalid.' in response.data, response.data)
+
+    def test_login4(self):       
+        """ tests login with empty user """
+        response = self.login("", "geheim")
+        self.assertTrue('This field is required.' in response.data, response.data)
+        
+    def test_login5(self):
+        """ tests login with empty pw """       
+        response = self.login("admin", "")
+        self.assertTrue('This field is required.' in response.data, response.data)
+
+    def test_interval1(self):
+        """ test the duration calucation """
+        interval1 = Interval()
+        interval1.start = datetime(2011, 4, 4, 19, 56, 23, 709000)
+        interval1.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1)
+        self.assertEqual(interval1.duration(), timedelta(0, 3600))
+        
+    def test_interval2(self):
+        """ tests duration calculation with empty stop-property """
+        interval1 = Interval()
+        interval1.start = datetime(2011, 4, 4, 19, 56, 23, 709000)
+        
+        pre = interval1.duration()
+        result = interval1.duration()
+        post = interval1.duration()
+        
+        self.assertTrue(pre < result < post, str(pre) + " < " + str(result) + " < " + str(post))
+    
+    def test_calc_durations(self):
+        """ tests sum durations from several intervals """
         interval1 = Interval()
         interval1.start = datetime(2011, 4, 4, 19, 56, 23, 709000)
         interval1.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1)
@@ -40,56 +138,28 @@ class FlaskTimeTrackerTestCase(unittest.TestCase):
         interval2 = Interval()
         interval2.start = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=2)
         interval2.stop = datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5)
-                
-        admin.intervals.append(interval1)
-        admin.intervals.append(interval2)
-        admin.save()
         
-    def test_initial_data(self):
-        User.objects().all().count()
-        user = User.objects().first()
-        self.assertEqual(user.username, "admin")
-        self.assertTrue(check_password_hash(user.pw_hash, "geheim"))
-        self.assertEqual(user.overtime, 0)
-        self.assertEqual(user.worktime, 28800)
-        self.assertEqual(len(user.intervals), 2)
+        result = flasktimetracker.calc_durations([interval1, interval2]);
+        self.assertEqual(timedelta(hours=4), result, str(interval1.duration()) + " " + str(interval2.duration()))
         
-        interval = user.intervals.pop()
-        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000)+ timedelta(hours=2))
-        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=5))
-        
-        interval = user.intervals.pop()
-        self.assertEqual(interval.start, datetime(2011, 4, 4, 19, 56, 23, 709000))
-        self.assertEqual(interval.stop, datetime(2011, 4, 4, 19, 56, 23, 709000) + timedelta(hours=1))
-       
-    def test_login_available(self):
-        self.assert_site_available("/login")
-        
-    def test_login_valid_user_pw(self):
-        response = self.login("admin","geheim")
-        self.assertTrue('logged in successfully.' in response.data, response.data)
+        result = flasktimetracker.calc_durations([]);
+        self.assertEqual(timedelta(hours=0), result, str(timedelta(hours=0)) + " " + str(result))
 
-    def test_login_logout(self):
-        response = self.login("admin","geheim")
-        self.assertTrue('logged in successfully.' in response.data, response.data)
-        response = self.logout()
-        self.assertTrue('logged out successfully.' in response.data, response.data)
-                       
-    def test_login_invalid_user(self):       
-        response = self.login("admin" + 'x',"geheim")
-        self.assertTrue('user/pw invalid.' in response.data, response.data)
-
-    def test_login_invalid_pw(self):       
-        response = self.login("admin", "geheim" + 'x')
-        self.assertTrue('user/pw invalid.' in response.data, response.data)
-
-    def test_login_empty_user(self):       
-        response = self.login("", "geheim")
-        self.assertTrue('This field is required.' in response.data, response.data)
+        result = flasktimetracker.calc_durations(None);
+        self.assertEqual(timedelta(hours=0), result, str(timedelta(hours=0)) + " " + str(result))
         
-    def test_login_empty_pw(self):       
-        response = self.login("admin", "")
-        self.assertTrue('This field is required.' in response.data, response.data)
+    def test_interval_edit(self):
+        """ tests the interval edit form """
+        self.login_admin()
+        response = self.get('/interval/edit/123')
+        self.assertTrue('404' in response.data, response.data)
         
+        interval = Interval.objects().first()
+        response = self.get("/interval/edit/" + str(interval.id))
+        self.assertTrue('404' not in response.data, response.data)
+        
+    def test_save_interval(self):
+        """ tests saving intervals """
+        self.assertTrue(False)
 if __name__ == '__main__':
     unittest.main()
